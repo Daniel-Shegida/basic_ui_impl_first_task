3d77a803fd12cfff4617f3afaec38cbec1a31fc0
daniel shegida <den.shegida@gmail.com>
2023-03-21 23:28:52 +0300

    feat(runner): edit run app mechanic

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: const Size(375, 872),
       useInheritedMediaQuery: true,
       builder: (_, child) {
         return MaterialApp(
