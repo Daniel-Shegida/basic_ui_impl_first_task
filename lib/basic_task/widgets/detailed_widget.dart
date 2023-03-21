6d6095b13bebc622a799dc4c2dd02b3ddd6d8827
daniel shegida <den.shegida@gmail.com>
2023-03-21 23:26:15 +0300

    feat(detailed): add detailed widget

---
+++ lib/basic_task/widgets/detailed_widget.dart
@@ -0,0 +1,26 @@
+import 'package:basic_ui_impl_first_task/assets/colors/project_colors.dart';
+import 'package:basic_ui_impl_first_task/assets/res/project_icons.dart';
+import 'package:basic_ui_impl_first_task/assets/strings/projects_strings.dart';
+import 'package:basic_ui_impl_first_task/assets/test_styles/project_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg_icons/flutter_svg_icons.dart';
+
+class DetailedWidget extends StatelessWidget {
+  const DetailedWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SvgIcon(
+          icon: SvgIconData(ProjectIcons.iDown),
+          color: ProjectColors.iconColor,
+        ),
+        SizedBox(width: 5.5.w,),
+        Text(ProjectStrings.detailed,
+            style: ProjectStyles.regularBlue.copyWith(fontSize: 12.sp)),
+      ],
+    );
+  }
+}
